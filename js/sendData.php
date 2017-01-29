7d4241c683390412f5d59ffcf34a82030ca82290
cmdjlee <joshleelee@gmail.com>
2017-01-28 16:43:32 -0800

    added comments to code

--- js/sendData.php
+++ js/sendData.php
@@ -1,4 +1,6 @@
 <?php
+  # This will send JSON data to be parsed in Javascript
+  # Reads in a json file in the same directory
   $file = json_decode(file_get_contents("mgh_data.json"));
   header("Content-type: application/json");
   print json_encode($file);
